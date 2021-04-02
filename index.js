9737043c2a8df491fcdfb2090d62c85aaf1de1bb
Ahmad Fadhel <ahmad.f.ameen@gmail.com>
2021-04-02 16:51:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // Code your solution in this file!
 const companyName = 'Scuber';
 let mostProfitableNeighborhood = 'Chelsea';
+<<<<<<< HEAD
 let companyCeo = 'Susan Smith';
+=======
+>>>>>>> ccebecf4f853d537fd1c775a262afd96ec18465a
